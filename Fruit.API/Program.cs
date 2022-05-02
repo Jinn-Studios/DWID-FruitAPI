c60f0794d2a27c10976ad182d523315d852534fe
Fletch <steven@jinnstudios.com>
2022-05-02 11:57:41 -0500

    Add SwaggerUI

--- Fruit.API/Program.cs
+++ Fruit.API/Program.cs
@@ -6,7 +6,16 @@ namespace FruitUI.API
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
             app.UseHttpsRedirection();
             app.MapControllers();
             app.Run();
